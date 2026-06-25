18579c4076886449b57cc2187e74e32c03b80e2d
papa-mux <kuloxmak@gmail.com>
2026-06-25 15:39:14 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/system/bin/bash
-curl -L -O "https://github.com/papa-mux/wrapper-for-termux/blob/main/libvulkan_wrapper.so"
-curl -L -O "https://github.com/papa-mux/wrapper-for-termux/blob/main/wrapper_icd.aarch64.json"
+curl -L -O "https://raw.githubusercontent.com/papa-mux/wrapper-for-termux/main/libvulkan_wrapper.so"
+curl -L -O "https://raw.githubusercontent.com/papa-mux/wrapper-for-termux/main/wrapper_icd.aarch64.json"
 cp libvulkan_wrapper.so $PREFIX/lib/
 cp wrapper_icd.aarch64.json $PREFIX/share/vulkan/icd.d/
 rm libvulkan_wrapper.so 
